722fcc87a4fe5869e7e8da111e608bcb991e6621
loganroe-se <loganroe388@gmail.com>
2024-10-31 13:01:45 -0500

    Add skills framework (skills list WIP)

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -16,4 +16,46 @@ window.addEventListener("DOMContentLoaded", (event) => {
             });
         });
     });
+
+    // ----------------------------------------
+    // Populate skills section via JSON
+    // ----------------------------------------
+    fetch("../Data/Skills.json")
+    .then((response) => {return response.json()})
+    .then((skills) => {loadSkills(skills)})
+    .catch((err) => {console.log("Error in fetching Skills: " + err)});
 });
+
+function loadSkills(skills) {
+    const techSkillsContainer = document.getElementById('tech-skills-container');
+    const generalSkillsContainer = document.getElementById('general-skills-container');
+
+    // Ensure both are cleared
+    techSkillsContainer.innerHTML = "";
+    generalSkillsContainer.innerHTML = "";
+
+    // Add technical skills via a helper
+    loadSkillsHelper(skills.Technical, techSkillsContainer);
+
+    // Add general skills via a helper
+    loadSkillsHelper(skills.General, generalSkillsContainer);
+}
+
+function loadSkillsHelper(skills, container) {
+    for (let i = 0; i < skills.length; i++) {
+        // Get all attributes
+        let name = skills[i].Name;
+        let image = skills[i].Image;
+
+        // Construct the new HTML element
+        let newSkill = document.createElement("div");
+        newSkill.classList.add("skill-card");
+        newSkill.innerHTML = `
+            <img class="skill-image" src=${image} alt=${name}>
+            <p>${name}</p>
+            `;
+        
+        // Add the new element to the container
+        container.appendChild(newSkill);
+    }
+}
